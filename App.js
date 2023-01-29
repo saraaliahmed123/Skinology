7ff43ad8d4ac173d40d037d30447d35d36b2732e
saraaliahmed123 <sara.ali.ahmed1@hotmail.com>
2023-01-29 14:03:15 +0000

    [added] Navigation
    - Stack navigation created
    - IndexPage created and added to stack

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import IndexScreen from './src/screens/IndexScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name='Index'
+          component={IndexScreen}
+          options={{title: "Index Page"}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
